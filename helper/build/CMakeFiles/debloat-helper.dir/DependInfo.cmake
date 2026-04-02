18d38e9804edad8d49c7ce06f727598bb4127d02
DavidsonRafaelK <davidson.rafael120@gmail.com>
2026-04-02 09:35:15 +0700

    Refactor code structure for improved readability and maintainability

---
+++ helper/build/CMakeFiles/debloat-helper.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/David/Desktop/windebloat/helper/src/filesystem.cpp" "CMakeFiles/debloat-helper.dir/src/filesystem.cpp.obj" "gcc" "CMakeFiles/debloat-helper.dir/src/filesystem.cpp.obj.d"
+  "C:/Users/David/Desktop/windebloat/helper/src/main.cpp" "CMakeFiles/debloat-helper.dir/src/main.cpp.obj" "gcc" "CMakeFiles/debloat-helper.dir/src/main.cpp.obj.d"
+  "C:/Users/David/Desktop/windebloat/helper/src/process.cpp" "CMakeFiles/debloat-helper.dir/src/process.cpp.obj" "gcc" "CMakeFiles/debloat-helper.dir/src/process.cpp.obj.d"
+  "C:/Users/David/Desktop/windebloat/helper/src/registry.cpp" "CMakeFiles/debloat-helper.dir/src/registry.cpp.obj" "gcc" "CMakeFiles/debloat-helper.dir/src/registry.cpp.obj.d"
+  "C:/Users/David/Desktop/windebloat/helper/src/service.cpp" "CMakeFiles/debloat-helper.dir/src/service.cpp.obj" "gcc" "CMakeFiles/debloat-helper.dir/src/service.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
